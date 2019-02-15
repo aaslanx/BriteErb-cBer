759b306527737179fc6864090b7129479a21da30
Alper Aslan <aslan@mushroomsiPhone.localdomain>
2019-02-15 02:08:32 -0500

    BrowserUtil, ConfiReader, Driver classes are created under Utilities package

---
+++ src/test/java/utilities/ConfigReader.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties;
+
+
+    // to read configuration file
+    static{
+        String path = "configuration.properties";
+        try {
+            FileInputStream file = new FileInputStream(path);
+            properties  = new Properties();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    // return key value
+    public static String getProperties(String key){
+        return properties.getProperty(key);
+    }
+
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,51 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+
+    private static WebDriver driver;
+
+    // driver getter
+    public static WebDriver getDriver() {
+
+        if(driver==null){
+            // browser Driver setup
+            switch(ConfigReader.getProperties("browser")) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                // if you need more browsers you may add from this line
+            }
+        }
+
+        //impilicity Wait setup
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+
+        return driver;
+    }
+
+    //quit Driver
+    public static  void closeDriver(){
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
